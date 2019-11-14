e5049dc472834e6acd43e800959030c55379d5fb
Frederic Petrot <Frederic.Petrot@imag.fr>
2019-11-14 11:40:32 +0100

    Installation dans /tmp/systemc-2.3.3

--- TPs/setup-ensimag.sh
+++ TPs/setup-ensimag.sh
@@ -3,6 +3,6 @@
 # Chemin d'installation de SystemC, tjs 2.3.2 en attendant les droits 
 # de mise à jour de /matieres/5MMMTSP
 # export SYSTEMCROOT=/matieres/5MMMTSP/tlm/systemc-2.3.2/
-export SYSTEMCROOT=/user/9/petrotf/opt/systemc-2.3.3
+export SYSTEMCROOT=/tmp/systemc-2.3.3
 # Réutilisation du cross-compilateur riscv de 4MMPCSEA
 export PATH=/matieres/4MMPCSEA/riscv/bin:$PATH
