cbd8a2d4944fd30a6f41ecb3e132569a2f4524db
Frederic Petrot <Frederic.Petrot@imag.fr>
2019-11-14 10:27:24 +0100

    Mise à jour chemin suite à changement de version de g++

--- TPs/setup-ensimag.sh
+++ TPs/setup-ensimag.sh
@@ -2,6 +2,7 @@
 
 # Chemin d'installation de SystemC, tjs 2.3.2 en attendant les droits 
 # de mise à jour de /matieres/5MMMTSP
-export SYSTEMCROOT=/matieres/5MMMTSP/tlm/systemc-2.3.2/
+# export SYSTEMCROOT=/matieres/5MMMTSP/tlm/systemc-2.3.2/
+export SYSTEMCROOT=/user/9/petrotf/opt/systemc-2.3.3
 # Réutilisation du cross-compilateur riscv de 4MMPCSEA
 export PATH=/matieres/4MMPCSEA/riscv/bin:$PATH
